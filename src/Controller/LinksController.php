9e422d789810e06115f94f15e01f2e18682a03cf
Roger Clotet <clotet89@gmail.com>
2019-02-25 20:00:06 +0100

    Change url back to links

--- src/Controller/LinksController.php
+++ src/Controller/LinksController.php
@@ -11,7 +11,7 @@ use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\Security;
 
 /**
- * @Route("/enllaços")
+ * @Route("/links")
  */
 class LinksController extends AbstractController
 {
