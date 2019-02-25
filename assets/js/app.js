2c93df726fa73823e2b90e1c663b2e2794263003
Roger Clotet <clotet89@gmail.com>
2019-02-25 00:08:09 +0000

    Links

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,6 +4,7 @@ import '../scss/main.scss';
 import '../scss/blog.scss';
 import '../scss/history.scss';
 import '../scss/album.scss';
+import '../scss/links.scss';
 import '../scss/admin.scss';
 
 import 'bootstrap';
---
+++ src/Entity/Link.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\LinkRepository")
+ */
+class Link
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $title;
+
+    /**
+     * @ORM\Column(type="string", length=2047, nullable=true)
+     */
+    private $description;
+
+    /**
+     * @ORM\Column(type="string", length=1023)
+     */
+    private $url;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(?string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getUrl(): ?string
+    {
+        return $this->url;
+    }
+
+    public function setUrl(string $url): self
+    {
+        $this->url = $url;
+
+        return $this;
+    }
+}
