87cbccc417cb3074ec22a25040cae12a368ee694
Roger Clotet <clotet89@gmail.com>
2019-03-19 02:38:13 +0100

    Remove console log for scroll position

--- assets/js/scroll.js
+++ assets/js/scroll.js
@@ -1,8 +1,6 @@
 window.addEventListener('scroll', (event) => {
     const header = document.getElementById('header');
 
-    console.log(window.scrollY);
-
     if (window.scrollY > 72 && !header.classList.contains('scroll')) {
         header.classList.add('scroll');
     } else if (window.scrollY <= 72 && header.classList.contains('scroll')) {
