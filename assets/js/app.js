9989ee6dc176500ad213f8b2cc803190b5e9dc57
Roger Clotet <clotet89@gmail.com>
2019-04-01 21:18:16 +0200

    Improve styling of about page

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,6 +2,7 @@ import 'bootstrap/less/bootstrap.less';
 
 import '../scss/main.scss';
 import '../scss/blog.scss';
+import '../scss/about.scss';
 import '../scss/history.scss';
 import '../scss/album.scss';
 import '../scss/links.scss';
